920e817e257ac4ef4008bda39fa9952a626ddbc1
Alain Pedrueza Basabe <104357836+Alain1812@users.noreply.github.com>
2023-10-15 19:36:10 +0200

    Update iniciosesion.php

--- app/iniciosesion.php
+++ app/iniciosesion.php
@@ -1,9 +1,7 @@
 <?php
-	ob_start();// para borrar el output buffer https://stackoverflow.com/questions/12654831/php-headers-already-sent-caused-by-session-start
+	ob_start();
 	session_start();
-	//conectamos Con el servidor
 	$conectar=@mysqli_connect("db","admin","test","database");
-	//verificamos la conexion
 	if(!$conectar){
 		echo"No Se Pudo Conectar Con El Servidor";
 	}else{
@@ -12,28 +10,23 @@
 			echo"No Se Encontro La Base De Datos";			
 		}
 	}
-	//----------------------------------------------------------------------------------------------------------------------------------------
-	//recuperar las variables
 	$nombre=$_POST['nombre'];
 	$dni=$_POST['dni'];
 	$contrasena=$_POST['contrasena'];
-	//----------------------------------------------------------------------------------------------------------------------------------------
-	$contrasenaUsuario="SELECT Contrasena FROM Usuario WHERE (DNI='$dni')"; //Consigue la contraseña del usuario
-	$usuario="SELECT * FROM Usuario WHERE (DNI='$dni')"; //Consigue el nombre (para ver si existe el usuario)
-	$nombreUsuario="SELECT Nombre FROM Usuario WHERE (DNI='$dni')"; //Consigue la contraseña del usuario
-	//----------------------------------------------------------------------------------------------------------------------------------------
-	$ejecutar=mysqli_query($conectar,$contrasenaUsuario); // ejecutar instruccion para conseguir la contraseña
-	$existeUsuario=mysqli_query($conectar,$usuario);      // ejecutar instruccion para saber si existe el usuario
-	$nombreSesion=mysqli_query($conectar,$nombreUsuario);  // ejecutar instruccion para conseguir nombre del usuario asociado al dni introducido
+	$contrasenaUsuario="SELECT Contrasena FROM Usuario WHERE (DNI='$dni')";
+	$usuario="SELECT * FROM Usuario WHERE (DNI='$dni')";
+	$nombreUsuario="SELECT Nombre FROM Usuario WHERE (DNI='$dni')"; 
+	$ejecutar=mysqli_query($conectar,$contrasenaUsuario); 
+	$existeUsuario=mysqli_query($conectar,$usuario);     
+	$nombreSesion=mysqli_query($conectar,$nombreUsuario);  
     if(isset($dni) && isset($contrasena)){                                           
-      if(mysqli_num_rows($existeUsuario)>0){ // es decir, si existe el usuario...
-        $cont=mysqli_fetch_array($ejecutar); // consigue su contraseña.
-        if(strcmp($cont[0],$contrasena) === 0){ // y la compara con la que ha introducido.      
-          /*SESION*/
+      if(mysqli_num_rows($existeUsuario)>0){ 
+        $cont=mysqli_fetch_array($ejecutar);
+        if(strcmp($cont[0],$contrasena) === 0){    
           $sesion=mysqli_fetch_array($nombreSesion);
           header("Location:areapersonal.php");
           $_SESSION['Usuario']=$sesion[0];
-          $_SESSION['DNI']=$dni;//mysqli_fetch_array($nombreSesion);
+          $_SESSION['DNI']=$dni;
           
         }else{
 ?>
@@ -67,7 +60,7 @@
     <input class="caja" type="password" name ="contrasena" id='contrasena'required><br>
     <input class="botones" type="submit" value="Iniciar Sesion" name="iniciar">
     <input class="botones"type="reset" value="Borrar datos" name="borrar">
-    <input class="botones" type="button" value="Volver página principal" name="volver" onclick="location.href='index.html'">
+    <input class="botones" type="button" value="Volver página principal" name="volver" onclick="location.href='index.php'">
   </section>
 
 </body>
